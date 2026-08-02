759ae088703cc40503744a8e7ca25b1c34bd34fb
Олексій Ієвенко <igrok1908@gmail.com>
2026-08-02 21:52:07 +0300

    fix: a little modify

--- src/woolf_agents/llm/config.py
+++ src/woolf_agents/llm/config.py
@@ -4,7 +4,8 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from .apikey import config
 
-
+class ConfigGoogleAPI(StrEnum):
+    GOOGLEGEMINI = config.get("GEMINIAPYKEY", None)
 
 class LLMProvider(StrEnum):
     OPENAI = "openai"
